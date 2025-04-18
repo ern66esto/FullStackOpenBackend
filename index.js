b0eb42b0b3d293f7e5eb2885a0aaed9514d1cc49
Gonzalez <eonl@gft.com>
2025-04-18 07:02:08 -0400

    adding frontend folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req.body) : ''});
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
+app.use(express.static('dist'));
 // const corsOptions = {
 //   origin: 'http://example.com',
 //   optionsSuccessStatus: 200
